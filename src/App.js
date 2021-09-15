133c048f533b8f19bdcc637f2a6b3ecccb315988
Daniel Bae <danibae91@icloud.com>
2021-09-14 21:16:05 -0700

    useState present from top-level component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react' 
-import './App.css';
+import React, { useState } from 'react' 
+import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Routes from './components/Routes'
+import './static/styles/App.css';
 
 function App() {
+
+  // initialize State 
+  const [colors, setColors] = useState([])
+
+  // addColor() 
+  const addColor = (color) => {
+    setColors(colors => [color, ...colors]) 
+  }
+
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Navbar /> 
+          <Routes colors={colors} addColor={addColor}/>
+      </BrowserRouter>
     </div>
   );
 }
